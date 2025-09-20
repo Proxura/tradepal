79e032c47c997be39081ffe18266b518416c3255
Benjamin Foreman <bfizznasty@gmail.com>
2025-09-20 16:21:43 -0400

    🐛 Fix incorrect callback URI to include /callback

--- login_test.py
+++ login_test.py
@@ -3,7 +3,7 @@ from schwab.auth import easy_client
 client = easy_client(
     api_key="your_client_id_here",
     app_secret="your_client_secret_here",
-    callback_url="https://127.0.0.1:8182",
+    callback_url="https://127.0.0.1:8182/callback",
     token_path="token.json"
 )
 
